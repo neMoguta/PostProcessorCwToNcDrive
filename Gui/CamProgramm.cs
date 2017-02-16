aa0fc741b2bbb1c59f0fc0c37c04d88441820057
NeMoguta <vyacheslav.mgtu@mail.ru>
2017-02-16 20:34:59 +0300

    gui

---
+++ Gui/CamProgramm.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gui
+{
+    class CamProgramm
+    {
+        public string OperationName { get; set; }
+        public string OperationParams { get; set; }
+
+        public static CamProgramm[] GetPersons()
+        {
+            return new []
+            {
+                new CamProgramm{OperationParams = "Sidorov", OperationName = "Sidor"},
+                new CamProgramm{OperationParams = "Petrov", OperationName = "Peter"},
+                new CamProgramm{OperationParams = "Victorov",OperationName = "Victor"}
+            };
+        }
+    }
+}
